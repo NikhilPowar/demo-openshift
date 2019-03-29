9e221bdc7a4a2f49699ed2ecb6d05c59d6695077
nikhilpowar <nikhilpowar16@gmail.com>
2019-03-29 16:23:29 +0530

    Changed database address

--- config.php
+++ config.php
@@ -1,9 +1,10 @@
 <?php
-	define('DB_SERVER', '172.30.45.190:3306');
+	define('DB_SERVER', '172.30.45.190');
 	define('DB_USERNAME', 'dummy');
 	define('DB_PASSWORD', 'dummy');
 	define('DB_DATABASE', 'thoughtblog');
-	$conn = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+	define('DB_PORT', '3306');
+	$conn = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE, DB_PORT);
 	if (!$conn) {
 		$_SESSION['login']="false";
 	}
