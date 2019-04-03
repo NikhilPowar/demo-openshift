ad688bf5649b3809a5ecc3dc8ef25ebba219c719
nikhilpowar <nikhilpowar16@gmail.com>
2019-04-03 10:55:28 +0530

    changed db name

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 	define('DB_SERVER', '10.131.26.183');
 	define('DB_USERNAME', 'dummy');
 	define('DB_PASSWORD', 'dummy');
-	define('DB_DATABASE', 'thoughtblog');
+	define('DB_DATABASE', 'sampledb');
 	define('DB_PORT', '3306');
 	$conn = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE, DB_PORT);
 	if (!$conn) {
